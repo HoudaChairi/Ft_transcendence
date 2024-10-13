642b565e256e36877c2fdf6ab60a510bc707d147
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:32:56 +0100

    more consumers

---
+++ backend/core/apps/chat/consumers.py
@@ -0,0 +1,5 @@
+import json
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+from authentication.models import Player
+from .models import Message
