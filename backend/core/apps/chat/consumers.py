dd0bd1aa26e81463f0a19345a35a1cd269cfac58
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:41:00 +0100

    update consumers

--- backend/core/apps/chat/consumers.py
+++ backend/core/apps/chat/consumers.py
@@ -7,18 +7,15 @@ from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-    self.room_name = self.scope['url_route']['kwargs']['room_name']
-    self.room_group_name = f'chat_{self.room_name}'
-
-    await self.channel_layer.group_add(
-        self.room_group_name,
-        self.channel_name
-    )
-
-    await self.accept()
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
     async def disconnect(self, close_code):
-
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -49,7 +46,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
         sender = event['sender']
-
         await self.send(text_data=json.dumps({
             'message': message,
             'sender': sender,
