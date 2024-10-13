a596330f4e91720f160c872f1dfda6865efedb24
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:32:30 +0100

    models done, add consumers.py

--- backend/core/apps/chat/models.py
+++ backend/core/apps/chat/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from authentication.models import Player
 
-
 class Message(models.Model):
     sender = models.ForeignKey(Player, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(Player, related_name='received_messages', on_delete=models.CASCADE)
