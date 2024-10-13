c9de1cc917a70ba0112ea65d5e644fcc6d1b70f2
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:33:59 +0100

    connect

--- backend/core/apps/chat/consumers.py
+++ backend/core/apps/chat/consumers.py
@@ -6,3 +6,13 @@ from .models import Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+    self.room_name = self.scope['url_route']['kwargs']['room_name']
+    self.room_group_name = f'chat_{self.room_name}'
+
+    await self.channel_layer.group_add(
+        self.room_group_name,
+        self.channel_name
+    )
+
+    await self.accept()
