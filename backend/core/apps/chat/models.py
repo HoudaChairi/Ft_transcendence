cfb7b0f40833fd22c73b6f34334e2074f1dd0b72
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:30:58 +0100

    class Message

--- backend/core/apps/chat/models.py
+++ backend/core/apps/chat/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from authentication.models import Player
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(Player, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Player, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
