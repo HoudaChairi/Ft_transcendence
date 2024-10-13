55932f70839acc4ebb924de625c235a0a8c72f29
mel-moun <mrymgithub@gmail.com>
2024-10-13 15:33:37 +0100

    Chat class

--- backend/core/apps/chat/consumers.py
+++ backend/core/apps/chat/consumers.py
@@ -3,3 +3,6 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from authentication.models import Player
 from .models import Message
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
