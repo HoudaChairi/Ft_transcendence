a7a241bfebce582bc7ab4323b70f53a7ccb951c9
mel-moun <mrymgithub@gmail.com>
2024-10-20 14:41:32 +0100

    added friends app

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'core.apps.authentication',
     'core.apps.chat',
     'core.apps.remote_auth',
+    'core.apps.friends',
 
     # 'core.apps.users',
     'rest_framework',
