21cb600bdf55f083d0065d74e123d6d4a2c6fae8
ThEWoLf882-42 <agimi@student.1337.ma>
2024-10-27 15:47:14 +0100

    fix

--- Frontend/js/Game.js
+++ Frontend/js/Game.js
@@ -750,15 +750,13 @@ class Game {
 	}
 
 	async #playUser(user) {
-		// this.#css2DObject.chatBtn.element.innerHTML = PLAY;
-		// this.#css2DObject.chatBtn.element.querySelector(
-		// 	'.select-new-username'
-		// ).textContent = `Start a Game With ${user}`;
-		// ['chatBtn', 'btnOverlay'].forEach(ele => {
-		// 	this.#scene.add(this.#css2DObject[ele]);
-		// });
-		// this.#removeUser(user);
-		this.#unblockUser(user);
+		this.#css2DObject.chatBtn.element.innerHTML = PLAY;
+		this.#css2DObject.chatBtn.element.querySelector(
+			'.select-new-username'
+		).textContent = `Start a Game With ${user}`;
+		['chatBtn', 'btnOverlay'].forEach(ele => {
+			this.#scene.add(this.#css2DObject[ele]);
+		});
 	}
 
 	async #blockUser(user) {
