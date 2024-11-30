f9a652938fb7093ee3f03e69dc5397eb511b6f3d
ThEWoLf882-42 <agimi@student.1337.ma>
2024-11-30 12:47:22 +0100

    Mekawis WORK

--- Frontend/js/Start.js
+++ Frontend/js/Start.js
@@ -1,19 +1,19 @@
 export const CHOICES = `<div class="card-container">
 	<div class="card-wrapper">
 		<div class="card" id="offline">
-			<img src="" class="card-content" alt="" />
+			<img src="/textures/png/Offline.png" class="card-content" alt="" />
 			<div class="card-title title-offline">Offline</div>
 		</div>
 	</div>
 	<div class="card-wrapper">
 		<div class="card" id="online">
-			<img src="" class="card-content" alt="" />
+			<img src="/textures/png/Online.png" class="card-content" alt="" />
 			<div class="card-title title-online">Online</div>
 		</div>
 	</div>
 	<div class="card-wrapper">
 		<div class="card" id="tournament">
-			<img src="" class="card-content" alt="" />
+			<img src="/textures/png/Tournament.png" class="card-content" alt="" />
 			<div class="card-title title-tournament">Tournament</div>
 		</div>
 	</div>
@@ -22,13 +22,13 @@ export const CHOICES = `<div class="card-container">
 export const OFFLINE = `<div class="card-container">
 	<div class="card-wrapper">
 		<div class="card" id="singleplayer">
-			<img src="" class="card-content" alt="" />
+			<img src="/textures/png/Singleplayer.png" class="card-content" alt="" />
 			<div class="card-title title-offline">Singleplayer</div>
 		</div>
 	</div>
 	<div class="card-wrapper">
 		<div class="card" id="multiplayer">
-			<img src="" class="card-content" alt="" />
+			<img src="/textures/png/Multiplayer.png" class="card-content" alt="" />
 			<div class="card-title title-online">Multiplayer</div>
 		</div>
 	</div>
