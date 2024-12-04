e8cbe9079f328dc7aab9d6597eadaea3067aaed1
ThEWoLf882-42 <agimi@student.1337.ma>
2024-12-04 15:11:19 +0100

    /textures/svg/Legend.svg

--- Frontend/js/Legend.js
+++ Frontend/js/Legend.js
@@ -2,5 +2,5 @@ export const LEGEND_CHAT = `Don’t Bother Me.`;
 
 export const LEGEND_LEADERBOARD = `Who is Not Retarded!?`;
 
-export const LEGEND = `<img class="legend-icon" alt="" src="/textures/svg/legend.svg" />
+export const LEGEND = `<img class="legend-icon" alt="" src="/textures/svg/Legend.svg" />
 <div class="dont-bother-me"></div>`;
